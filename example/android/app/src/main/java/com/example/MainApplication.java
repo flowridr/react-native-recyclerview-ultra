d875664b2bddb8e667c449a17b26c83eb669f778
Lasse Laasonen <>
2019-09-13 12:36:45 +0300

    Revert "update to react native 0.60.5"
    This reverts commit 3f6c2e150b47b2113e52587ed9800a7c65718663.

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -1,16 +1,13 @@
 package com.example;
 
 import android.app.Application;
-import android.util.Log;
-import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
+
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-// import com.github.godness84.RNRecyclerViewList.RNRecyclerviewListPackage;
+import com.github.godness84.RNRecyclerViewList.RNRecyclerviewListPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,11 +22,10 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
-      return packages;
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+		  new RNRecyclerviewListPackage()
+      );
     }
 
     @Override
